9aef9affae21349f5c21ad0593076b5db17cf057
PavelBlend <stalkermodkytia@yandex.ru>
2018-12-29 19:45:54 +0300

    fix bug in resume baking mesh

--- jet_fluids/bake.py
+++ jet_fluids/bake.py
@@ -119,68 +119,74 @@ class JetFluidBakeMesh(bpy.types.Operator):
             gridOrigin=(origin_x, origin_z, origin_y),
             domainSizeX=self.domain_size_x
         )
-        frame_index = scn.frame_start
-        while frame_index <= scn.frame_end:
-            print('frame', frame_index)
-            file_path = '{}particles_{}.bin'.format(
-                bpy.path.abspath(domain.jet_fluid.cache_folder),
-                frame_index
-            )
-            if not os.path.exists(file_path):
-                frame_index += 1
-                continue
-            print('open particles file')
-            particles_file = open(file_path, 'rb')
-            particles_data = particles_file.read()
-            particles_file.close()
-            print('read particles')
-            p = 0
-            particles_count = struct.unpack('I', particles_data[p : p + 4])[0]
-            p += 4
-            points = []
-            for particle_index in range(particles_count):
-                particle_position = struct.unpack('3f', particles_data[p : p + 12])
-                p += 36    # skip velocities and forces
-                points.append(particle_position)
-
-            print('create converter')
-            converter = pyjet.SphPointsToImplicit3(2.0 * solv.gridSpacing.x, 0.5)
-            print('convert')
-            converter.convert(points, grid)
-            print('meshing')
-            surface_mesh = pyjet.marchingCubes(
-                grid,
-                (solv.gridSpacing.x, solv.gridSpacing.y, solv.gridSpacing.z),
-                (0, 0, 0),
-                0.0,
-                pyjet.DIRECTION_ALL
-            )
-            print('save verts')
-            coef = self.domain.jet_fluid.resolution / self.domain.jet_fluid.resolution_mesh
-            bin_mesh_data = bytearray()
-            points_count = surface_mesh.numberOfPoints()
-            bin_mesh_data.extend(struct.pack('I', points_count))
-            for point_index in range(points_count):
-                point = surface_mesh.point(point_index)
-                bin_mesh_data.extend(struct.pack('3f', point.x * coef, point.y * coef, point.z * coef))
-
-            print('save tris')
-            triangles_count = surface_mesh.numberOfTriangles()
-            bin_mesh_data.extend(struct.pack('I', triangles_count))
-            for triangle_index in range(triangles_count):
-                tris = surface_mesh.pointIndex(triangle_index)
-                bin_mesh_data.extend(struct.pack('3I', tris.x, tris.y, tris.z))
-
-            print('write file')
+        for frame_index in range(scn.frame_start, scn.frame_end + 1):
             file_path = '{}mesh_{}.bin'.format(
                 bpy.path.abspath(self.domain.jet_fluid.cache_folder),
                 frame_index
             )
-            file = open(file_path, 'wb')
-            file.write(bin_mesh_data)
-            file.close()
-            print('save mesh end')
-            frame_index += 1
+            if os.path.exists(file_path):
+                print('skip frame', frame_index)
+                continue
+            else:
+                print('frame', frame_index)
+                file_path = '{}particles_{}.bin'.format(
+                    bpy.path.abspath(domain.jet_fluid.cache_folder),
+                    frame_index
+                )
+                if not os.path.exists(file_path):
+                    print('can\'t find particles file in {} frame'.format(frame_index))
+                    continue
+                print('open particles file')
+                particles_file = open(file_path, 'rb')
+                particles_data = particles_file.read()
+                particles_file.close()
+                print('read particles')
+                p = 0
+                particles_count = struct.unpack('I', particles_data[p : p + 4])[0]
+                p += 4
+                points = []
+                for particle_index in range(particles_count):
+                    particle_position = struct.unpack('3f', particles_data[p : p + 12])
+                    p += 36    # skip velocities and forces
+                    points.append(particle_position)
+    
+                print('create converter')
+                converter = pyjet.SphPointsToImplicit3(2.0 * solv.gridSpacing.x, 0.5)
+                print('convert')
+                converter.convert(points, grid)
+                print('meshing')
+                surface_mesh = pyjet.marchingCubes(
+                    grid,
+                    (solv.gridSpacing.x, solv.gridSpacing.y, solv.gridSpacing.z),
+                    (0, 0, 0),
+                    0.0,
+                    pyjet.DIRECTION_ALL
+                )
+                print('save verts')
+                coef = self.domain.jet_fluid.resolution / self.domain.jet_fluid.resolution_mesh
+                bin_mesh_data = bytearray()
+                points_count = surface_mesh.numberOfPoints()
+                bin_mesh_data.extend(struct.pack('I', points_count))
+                for point_index in range(points_count):
+                    point = surface_mesh.point(point_index)
+                    bin_mesh_data.extend(struct.pack('3f', point.x * coef, point.y * coef, point.z * coef))
+    
+                print('save tris')
+                triangles_count = surface_mesh.numberOfTriangles()
+                bin_mesh_data.extend(struct.pack('I', triangles_count))
+                for triangle_index in range(triangles_count):
+                    tris = surface_mesh.pointIndex(triangle_index)
+                    bin_mesh_data.extend(struct.pack('3I', tris.x, tris.y, tris.z))
+    
+                print('write file')
+                file_path = '{}mesh_{}.bin'.format(
+                    bpy.path.abspath(self.domain.jet_fluid.cache_folder),
+                    frame_index
+                )
+                file = open(file_path, 'wb')
+                file.write(bin_mesh_data)
+                file.close()
+                print('save mesh end')
         return {'FINISHED'}
 
 
