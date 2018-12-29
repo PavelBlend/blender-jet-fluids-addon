17702f4c43dfb6016b24c4ecc41178f7ef4d1938
PavelBlend <stalkermodkytia@yandex.ru>
2018-12-29 19:50:32 +0300

    Demo v0.0.2

--- jet_fluids/__init__.py
+++ jet_fluids/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     'name': 'Jet-Fluids',
     'author': 'Pavel_Blend',
-    'version': ('Demo', 0, 0, 1),
+    'version': ('Demo', 0, 0, 2),
     'blender': (2, 79, 0),
     'category': 'Animation'
 }
