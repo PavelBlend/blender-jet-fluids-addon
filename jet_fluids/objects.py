d3c167d76d290d0fde81c0fe9f2e36d18b7333b7
PavelBlend <stalkermodkytia@yandex.ru>
2018-12-29 23:31:37 +0300

    added comments in objects module

--- jet_fluids/objects.py
+++ jet_fluids/objects.py
@@ -6,7 +6,10 @@ from . import create
 
 class JetFluidsProperties(bpy.types.PropertyGroup):
     bpy_type = bpy.types.Object
+
     is_active = bpy.props.BoolProperty(default=False)
+
+    # simulate props
     items = [
         ('APIC', 'APIC', ''),
         ('PIC', 'PIC', ''),
@@ -17,28 +20,75 @@ class JetFluidsProperties(bpy.types.PropertyGroup):
     )
     resolution = bpy.props.IntProperty(default=30, name='Simulate Resolution')
     resolution_mesh = bpy.props.IntProperty(default=30, name='Mesh Resolution')
-    particles_count = bpy.props.FloatProperty(default=2.0, name='Particles', min=1.0)
+    particles_count = bpy.props.FloatProperty(
+        default=1.0,
+        name='Particles',
+        min=1.0
+    )
+
+    # cache props
+    cache_folder = bpy.props.StringProperty(
+        default='',
+        name='Cache Folder',
+        subtype='DIR_PATH'
+    )
+
+    # world props
     viscosity = bpy.props.FloatProperty(default=0.0, name='Viscosity')
+
+    # emitter props
     emitter = bpy.props.StringProperty(default='', name='Emitter')
     velocity = bpy.props.FloatVectorProperty(default=(0, 0, 0), name='Velocity')
     one_shot = bpy.props.BoolProperty(default=False, name='One Shot')
+
+    # collider props
     collider = bpy.props.StringProperty(default='', name='Collider')
-    create_mesh = bpy.props.BoolProperty(default=True, name='Create Mesh', update=create.update_par_object)
+
+    # create props
+    create_mesh = bpy.props.BoolProperty(
+        default=True,
+        name='Create Mesh',
+        update=create.update_par_object
+    )
     mesh_object = bpy.props.StringProperty(default='', name='Mesh')
+    create_particles = bpy.props.BoolProperty(
+        default=False,
+        name='Create Particles',
+        update=create.update_par_object
+    )
+    particles_object = bpy.props.StringProperty(default='', name='Particles')
+
+    # debug props
     show_particles = bpy.props.BoolProperty(default=True, name='Show Particles')
-    cache_folder = bpy.props.StringProperty(default='', name='Cache Folder', subtype='DIR_PATH')
-    color_1 = bpy.props.FloatVectorProperty(default=(0.0, 0.0, 1.0), name='Color 1', subtype='COLOR', max=1.0, min=0.0)
-    color_2 = bpy.props.FloatVectorProperty(default=(0.0, 1.0, 1.0), name='Color 2', subtype='COLOR', max=1.0, min=0.0)
-    max_velocity = bpy.props.FloatProperty(default=10.0, name='Max Velocity', min=0.001)
     items = [
         ('VELOCITY', 'Velocity', ''),
         ('SINGLE_COLOR', 'Single Color', '')
     ]
     color_type = bpy.props.EnumProperty(
-        items=items, name='Color', default='VELOCITY', update=create.update_particles_cache
+        items=items,
+        name='Color',
+        default='VELOCITY',
+        update=create.update_particles_cache
     )
-    particles_object = bpy.props.StringProperty(default='', name='Particles')
-    create_particles = bpy.props.BoolProperty(default=False, name='Create Particles', update=create.update_par_object)
+    color_1 = bpy.props.FloatVectorProperty(
+        default=(0.0, 0.0, 1.0),
+        name='Color 1',
+        subtype='COLOR',
+        max=1.0,
+        min=0.0
+    )
+    color_2 = bpy.props.FloatVectorProperty(
+        default=(0.0, 1.0, 1.0),
+        name='Color 2',
+        subtype='COLOR',
+        max=1.0,
+        min=0.0
+    )
+    max_velocity = bpy.props.FloatProperty(
+        default=10.0,
+        name='Max Velocity',
+        min=0.001
+    )
 
 
 __CLASSES__ = [
