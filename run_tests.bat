8556ee31ce007917f7eb8b2b0c5ed3ff39aebc9a
PavelBlend <stalkermodkytia@yandex.ru>
2020-04-03 21:33:44 +0300

    added tests

---
+++ run_tests.bat
@@ -0,0 +1,2 @@
+"C:\Program Project\Blender 2.82\blender.exe" -b --python pic_solver_tests.py
+pause
---
+++ tests.py
@@ -0,0 +1,46 @@
+import os
+
+import bpy
+
+
+solvers = ['FLIP', 'PIC', 'APIC']
+for solver in solvers:
+    bpy.ops.mesh.primitive_cube_add(
+        size=2, enter_editmode=False, location=(0, 0, 0)
+    )
+    domain_object = bpy.context.object
+    bpy.ops.jet_fluid.add()
+    domain_object.jet_fluid.object_type = 'DOMAIN'
+    domain_object.jet_fluid.resolution = 20
+    domain_object.jet_fluid.solver_type = solver
+    cache_folder = os.path.join(
+        os.path.abspath(os.curdir), 'pic_solver_tests_cache'
+    )
+    cache_folder += os.sep
+    if not os.path.exists(cache_folder):
+        os.makedirs(cache_folder)
+    domain_object.jet_fluid.cache_folder = cache_folder
+    
+    bpy.context.scene.frame_start = 0
+    bpy.context.scene.frame_end = 5
+    
+    bpy.ops.mesh.primitive_ico_sphere_add(
+        subdivisions=3, radius=0.5, enter_editmode=False, location=(0, 0, 0)
+    )
+    emitter_object = bpy.context.object
+    bpy.ops.jet_fluid.add()
+    emitter_object.jet_fluid.object_type = 'EMITTER'
+    emitter_object.jet_fluid.particles_count = 2.0
+    
+    bpy.context.view_layer.objects.active = domain_object
+    bpy.ops.jet_fluid.bake_particles()
+    bpy.ops.jet_fluid.bake_mesh()
+    bpy.ops.jet_fluid.create_particle_system()
+    bpy.ops.jet_fluid.reset_particles()
+    bpy.ops.jet_fluid.reset_mesh()
+    bpy.ops.jet_fluid.reset_physic_cache()
+
+print('\n' * 5)
+print('=' * 79)
+print('FINISH!')
+print('=' * 79)
