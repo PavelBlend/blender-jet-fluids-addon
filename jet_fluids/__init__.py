e7a8e09387aac49292fd57ccc43d1f2974ba3539
Pavel_Blend <stalkermodkytia@yandex.ru>
2022-08-06 19:27:03 +0300

    v0.1.0

--- jet_fluids/__init__.py
+++ jet_fluids/__init__.py
@@ -1,8 +1,8 @@
 bl_info = {
     'name': 'Jet-Fluids',
     'author': 'Pavel_Blend',
-    'version': ('demo', 0, 0, 5),
-    'blender': (2, 81, 0),
+    'version': (0, 1, 0),
+    'blender': (3, 2, 1),
     'category': 'Animation',
     'location': 'Properties > Physics > Jet Fluid',
     'warning': 'Demo version',
